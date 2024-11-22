17aaf48f778ee027c291e62d7df9887cca36446c
kishore kasula <kishorekasula123@gmail.com>
2024-11-22 20:12:45 +0530

    first commit

---
+++ src/app/payment-details/payment-detail-form/payment-detail-form.component.ts
@@ -0,0 +1,56 @@
+import { Component } from '@angular/core';
+import { PaymentDetailService } from 'src/app/shared/payment-detail.service';
+import { NgForm } from "@angular/forms";
+import { PaymentDetail } from 'src/app/shared/payment-detail.model';
+import { ToastrService } from 'ngx-toastr';
+
+@Component({
+  selector: 'app-payment-detail-form',
+  templateUrl: './payment-detail-form.component.html',
+  styles: [
+  ]
+})
+export class PaymentDetailFormComponent {
+
+  constructor(public service: PaymentDetailService, private toastr: ToastrService) {
+  }
+
+  onSubmit(form: NgForm) {
+     debugger;
+    this.service.formSubmitted = true
+    if (form.valid) {
+      if (this.service.formData.paymentDetailId == 0)
+        this.insertRecord(form)
+      else
+        this.updateRecord(form)
+    }
+
+  }
+
+  insertRecord(form: NgForm) {
+    debugger;
+    this.service.postPaymentDetail()
+      .subscribe({
+        next: res => {
+          debugger;
+          this.service.list = res as PaymentDetail[]
+          this.service.resetForm(form)
+          this.toastr.success('Inserted successfully', 'Payment Detail Register')
+        },
+        error: err => { console.log(err) }
+      })
+  }
+  updateRecord(form: NgForm) { 
+    debugger;
+    this.service.putPaymentDetail()
+      .subscribe({
+        next: res => {
+          this.service.list = res as PaymentDetail[]
+          this.service.resetForm(form)
+          this.toastr.info('Updated successfully', 'Payment Detail Register')
+        },
+        error: err => { console.log(err) }
+      })
+   }
+
+}
